8d58dd5505ffceedd4495fb42a2b5331fdf22646
Ruru <renehinrichsen@yahoo.dk>
2025-09-05 10:50:45 +0200

    Moved some classes to a ClassLibrary

---
+++ MySimpleBankGUI/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySimpleBankGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBalanceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // Calls the logic to check balance
+            MessageBox.Show("Check Balance button clicked!");
+        }
+    }
+}
